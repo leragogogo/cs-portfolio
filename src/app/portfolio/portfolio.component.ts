23c0030ae101295ded578c29b91dbc56857c13ab
Valeriia Gogotova <gogotovavaleria19@gmail.com>
2024-06-19 15:52:55 +0200

    all components implemented

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {Routes} from '@angular/router';
+
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
